9942dcc146e9be662191a6e275d86d49286dd9e9
Sayak Chakrabarty <pidnas94335@gmail.com>
2020-12-15 17:08:09 +0530

    Update Basicmath.py

--- Basicmath.py
+++ Basicmath.py
@@ -182,3 +182,28 @@ def nextnum(s):
 			if m > int(s):#check if the formed number crosses s
 				return m
 	return "-1"#the given num is the lasrgest possible with those digits and complexity is O(n^2 logn)
+
+# Function to return the square root of  
+# a number using Newtons method  
+def squareRoot(n, l) : 
+  
+    # Assuming the sqrt of n as n only  
+    x = n  
+  
+    # To count the number of iterations  
+    count = 0 
+  
+    while (1) : 
+        count += 1 
+  
+        # Calculate more closed x  
+        root = 0.5 * (x + (n / x))  
+  
+        # Check for closeness  
+        if (abs(root - x) < l) : 
+            break 
+  
+        # Update root  
+        x = root 
+  
+    return root
